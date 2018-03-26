a56a299ad1e22eef2d8fb37d3d645b566d79718e
Olivier DEBON <odebon@swift-tools.net>
2018-03-26 19:39:57 +0200

    Promys now print text information onto splash.

--- stage3/01-system/00-run.sh
+++ stage3/01-system/00-run.sh
@@ -2,6 +2,7 @@
 
 install -m 644 files/config.txt ${ROOTFS_DIR}/boot/
 install -m 644 files/cmdline.txt ${ROOTFS_DIR}/boot/
+install -m 644 files/info.txt ${ROOTFS_DIR}/boot/
 
 install -m 755 files/rc.local   ${ROOTFS_DIR}/etc/rc.local
 
--- stage3/03-promys/00-run.sh
+++ stage3/03-promys/00-run.sh
@@ -2,4 +2,5 @@
 
 install -v -d ${ROOTFS_DIR}/opt/promys
 install -m 755 files/promys     ${ROOTFS_DIR}/opt/promys/
+install -m 644 files/lucon.ttf  ${ROOTFS_DIR}/opt/promys/
 install -m 644 files/splash.jpg ${ROOTFS_DIR}/boot
